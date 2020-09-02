eac5550de31585a525ca9cc7fa1dcae9f802ccca
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-09-02 16:00:08 -0400

    api is working

---
+++ backend/app/controllers/carts_controller.rb
@@ -0,0 +1,31 @@
+class CartsController < ApplicationController
+
+  def index
+    carts = Cart.all
+    render json: carts
+  end
+
+  def create
+    cart = Cart.create(cart_params)
+    render json: cart
+  end
+
+  def update
+    cart = Cart.find_by!(params[:id])
+    cart.update(cart_params)
+    render json: cart
+  end
+
+  def destroy
+    cart = Cart.find_by!(params[:id])
+    cart.destroy!
+    render json: {}
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:user_id, :item_id, :quantity)
+  end
+
+end
