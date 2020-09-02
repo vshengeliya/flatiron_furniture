d32393e6001e036329054273e02d326a5bc1592c
github username <github email address>
2020-09-02 17:13:12 -0400

    sinc issues

--- backend/config/boot.rb
+++ backend/config/boot.rb
@@ -1,8 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-<<<<<<< HEAD
 # require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-=======
-#require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
->>>>>>> ffd8628b4ffd4deb55959b661ca555b6b6b529f6
+
