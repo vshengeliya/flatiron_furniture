de07adb8aa2a56aa984d7a10fa6a2660b40111f3
github username <github email address>
2020-09-02 20:01:59 -0400

    updated some tests

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -10,6 +10,11 @@ class UsersController < ApplicationController
     render json: user
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
