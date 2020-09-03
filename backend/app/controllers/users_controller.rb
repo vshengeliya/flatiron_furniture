8d4a8123f5f3b89143160660c40afeb92f02a634
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-09-03 05:40:33 -0400

    CRUD fully tested on user api

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
+    puts user
     user.update(user_params)
     render json: user
   end
--- frontend/public/test/testindex.js
+++ frontend/public/test/testindex.js
@@ -23,6 +23,13 @@ document.addEventListener("DOMContentLoaded", e => {
     const renderUser = (user) => {
         const userDiv = document.createElement("div")
         userDiv.dataset.id = user.id
+        userDiv.dataset.username = user.username
+        userDiv.dataset.password = user.password
+        userDiv.dataset.first_name = user.first_name
+        userDiv.dataset.last_name = user.last_name
+        userDiv.dataset.address = user.address
+        userDiv.dataset.city = user.city
+        userDiv.dataset.state = user.state
         userDiv.innerHTML = `
         <p>${user.first_name}</p>      
         <button>Update</button>  
@@ -34,7 +41,7 @@ document.addEventListener("DOMContentLoaded", e => {
     const submitHandler = () => {
         document.addEventListener("submit", e => {
             e.preventDefault()
-            if (e.target.id === "user-form"){
+            if (e.target.id === "new-user-form"){
                 const button = e.target
                 submitUserForm(button)
             }
@@ -46,6 +53,8 @@ document.addEventListener("DOMContentLoaded", e => {
             if (e.target.innerText === "Delete") {
                 deleteUser(e.target)
             } else if (e.target.innerText === "Update") {
+                renderUpdateUserForm(e.target)
+            } else if (e.target.innerText === "Update User") {
                 updateUser(e.target)
             }
         })
@@ -73,6 +82,7 @@ document.addEventListener("DOMContentLoaded", e => {
         fetch(usersUrl, packet)
             .then(res => res.json())
             .then(newItem => renderUser(newItem))
+            .then(() => form.reset())
     }
 
     const deleteUser = (e) => {
@@ -91,24 +101,37 @@ document.addEventListener("DOMContentLoaded", e => {
     }
 
     const updateUser = (e) => {
-        const userId = e.parentNode.dataset.id
+        const form = e.parentNode
+        const userId = form.dataset.id
+
+        const data = {
+            username: form.username.value,
+            password: form.password.value,
+            first_name: form.first_name.value,
+            last_name: form.last_name.value,
+            address: form.address.value,
+            city: form.city.value,
+            state: form.state.value
+        }
+
         const packet = {
-            method: "GET",
+            method: "PATCH",
             headers: {
                 "content-type": "application/json",
                 "accept": "application/json",
-            }
+            },
+            body: JSON.stringify(data)
         }
         fetch(usersUrl + userId, packet)
             .then(res => res.json())
-            .then(user => renderForm(user))
+            .then(() => renderUsers())
     }
 
-    const renderForm = (user= defaultUser ) => {
+    const renderNewUserForm = (user = defaultUser) => {
         const formContainer = document.getElementById("forms-container")
         formContainer.innerHTML = ''
         const userForm = document.createElement("form")
-        userForm.id = "user-form"
+        userForm.id = "new-user-form"
 
         userForm.innerHTML = `
             <input name="username" type="text" placeholder="Username" value="${user.username}"><br>
@@ -118,7 +141,28 @@ document.addEventListener("DOMContentLoaded", e => {
             <input name="address" type="text" placeholder="Address" value="${user.address}"><br>
             <input name="city" type="text" placeholder="city" value="${user.city}"><br>
             <input name="state" type="text" placeholder="state" value="${user.state}"><br>
-          
+            <input type="submit">
+        `
+        formContainer.append(userForm)
+    }
+
+    const renderUpdateUserForm = (button) => {
+        const user = button.parentNode.dataset
+        const formContainer = document.getElementById("forms-container")
+        formContainer.innerHTML = ''
+        const userForm = document.createElement("form")
+        userForm.dataset.id = user.id
+        userForm.id = "update-user-form"
+
+        userForm.innerHTML = `
+            <input name="username" type="text" placeholder="Username" value="${user.username}"><br>
+            <input name="password" type="text" placeholder="Password" value="${user.password}"><br>
+            <input name="first_name" type="text" placeholder="First-name" value="${user.first_name}"><br>
+            <input name="last_name" type="text" placeholder="Last Name" value="${user.last_name}"><br>
+            <input name="address" type="text" placeholder="Address" value="${user.address}"><br>
+            <input name="city" type="text" placeholder="city" value="${user.city}"><br>
+            <input name="state" type="text" placeholder="state" value="${user.state}"><br>
+            <button>Update User</button>
         `
         formContainer.append(userForm)
     }
@@ -126,6 +170,6 @@ document.addEventListener("DOMContentLoaded", e => {
     submitHandler()
     renderUsers()
     clickHandler()
-    renderForm()
+    renderNewUserForm()
 
 })
