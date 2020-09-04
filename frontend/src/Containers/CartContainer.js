6b37d5d3182689bf0f3143accf49a90744ec58ff
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-09-04 16:49:27 -0400

    sync issues

---
+++ frontend/src/Containers/CartContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Item from '../Components/Item'
+
+
+class CartContainer extends React.Component {
+
+
+
+         render()
+         {
+             return (
+                 <>
+
+                 </>
+             )
+         }
+
+ }
+    
+    
+export default CartContainer
