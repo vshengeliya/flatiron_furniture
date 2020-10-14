fc710ffb5fb2ad8fcda1db9ff34ea2030226285e
Veronika Dodda <v.shengeliya@gmail.com>
2020-10-14 11:13:10 -0400

    first commit

--- frontend/src/Containers/LoginContainer.js
+++ frontend/src/Containers/LoginContainer.js
@@ -29,7 +29,7 @@ class LoginContainer extends React.Component {
     render() {
         return(
             <div>
-                {this.props.user.id ?
+                {this.props.user && this.props.token?
                     <div>
                         <div className="user-container"><h3>Welcome Back {this.props.user.first_name}</h3>
                             <div className="my-information"><h5>My Information</h5>
